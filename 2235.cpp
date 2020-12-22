4dfb5e34c694eecc8ab2a58aa22314d2f209fa00
guga <guga.tomas10@gmail.com>
2020-12-22 09:55:51 -0300

    no comments

--- 2235.cpp
+++ 2235.cpp
@@ -79,5 +79,12 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(a == b || a == c || b == c) cout << "S" << endl;
+    else if(a + b == c || a + c == b || b + c == a) cout << "S" << endl;
+    else cout << "N" << endl;
+
     return 0;
 }
