243f49e1e699337002c46346719efcad28f0ad56
Guga <54151974+gustavo-tomas@users.noreply.github.com>
2021-01-03 11:32:43 -0300

    Create 2840.cpp

---
+++ 2840.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    int r, l;
+    cin >> r >> l;
+    cout << (int) (l / (4 * 3.1415 * r * r * r * 1/3) ) << endl;
+    return 0;
+}
