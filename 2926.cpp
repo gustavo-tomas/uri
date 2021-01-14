bca91c84da4047530765b4c2b62756d6b7087383
Guga <54151974+gustavo-tomas@users.noreply.github.com>
2021-01-14 20:10:58 -0300

    so it is christmas

---
+++ 2926.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    int n;
+    cin >> n;
+    cout << "Ent";
+    for(int i=0; i<n; i++) cout << "a";
+    cout << "o eh N";
+    for(int i=0; i<n; i++) cout << "a";
+    cout << "t";
+    for(int i=0; i<n; i++) cout << "a";
+    cout << "l!" << endl;
+ 
+    return 0;
+}
