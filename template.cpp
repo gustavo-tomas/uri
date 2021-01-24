1ab8d6f18d5183d03297d9ba71df5f1f1ee62237
Guga <54151974+gustavo-tomas@users.noreply.github.com>
2021-01-24 20:39:03 -0300

    upgraded bin search

--- template.cpp
+++ template.cpp
@@ -88,15 +88,18 @@ using namespace std;
 // }
 
 // busca binaria - mudar array type para conveniencia
-// int bin_search(array_type v, int val){
-//     int l = 0, r = v.size()-1, mid=0, flag=0;
-//     while((l <= r) && !flag){
+// int bin_search(vector<int> v, int val){
+//     int l = 0, r = v.size()-1, mid=0, flag=0, res = -1;
+//     while(l <= r){
 //         mid = (r + l)/2;
 //         if(val < v[mid]) r = mid-1;
 //         else if(val > v[mid]) l = mid+1;
-//         else flag = 1;
+//         else{
+//             res = mid;
+//             r = mid - 1;
+//         }
 //     }
-//     return mid;
+//     return res;
 // }
  
 // https://stackoverflow.com/questions/22746429/c-decimal-to-binary-converting/22746526
