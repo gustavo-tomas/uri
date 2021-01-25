3537cb6e4aaf5be9b0f4b2e47cd75d752b15c027
Guga <54151974+gustavo-tomas@users.noreply.github.com>
2021-01-25 08:49:59 -0300

    padding numbers in ccccccccccccccccc

---
+++ 2757.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ 
+    // ios_base::sync_with_stdio(false); 
+    // cin.tie(NULL);
+    // cout.tie(NULL);
+    
+    int a, b, c;
+    cin >> a >> b >> c;
+    printf("A = %d, B = %d, C = %d\n", a, b, c);
+    printf("A = %10d, B = %10d, C = %10d\n", a, b, c);
+    printf("A = %010d, B = %010d, C = %010d\n", a, b, c);
+    printf("A = %-10d, B = %-10d, C = %-10d\n", a, b, c);
+    
+    return 0;
+}
