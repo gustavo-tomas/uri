91cd143f0224b48eba46289eb553d2a35c04fb6b
Guga <54151974+gustavo-tomas@users.noreply.github.com>
2021-01-28 11:43:12 -0300

    containers gone wrong

---
+++ 2395.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ 
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b, c, x, y, z;
+    cin >> a >> b >> c >> x >> y >> z;
+
+    cout << (x/a) * (y/b) * (z/c) << endl;
+   
+    return 0;
+}
