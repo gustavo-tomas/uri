3e814a748a5f172e8f19bd0b36211b765cfb1abc
Guga <54151974+gustavo-tomas@users.noreply.github.com>
2021-01-28 18:14:10 -0300

    dont forget to drink water

---
+++ 2416.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ 
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int c, n;
+    cin >> c >> n;
+    cout << c % n << endl;
+   
+    return 0;
+}
