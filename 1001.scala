e6a552d87e5e5193ce56d64751c4e30c73aff297
Guga <54151974+gustavo-tomas@users.noreply.github.com>
2021-02-14 10:25:45 -0300

    pain to read input ngl

---
+++ 1001.scala
@@ -0,0 +1,11 @@
+object Main {
+
+    def main(args: Array[String]) {
+
+        var x: Int = scala.io.StdIn.readInt()
+        var y: Int = scala.io.StdIn.readInt()
+        x += y
+        println("X = " + x)
+    }
+
+}
