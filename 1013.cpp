c985afe2beaab1138316bafe9b233a23ea3d64e6
Guga <54151974+gustavo-tomas@users.noreply.github.com>
2021-03-08 18:44:46 -0300

    the greatest

---
+++ 1013.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a, b, c, g;
+
+    cin >> a >> b >> c;
+    g = max({a,b,c});
+
+    cout << g << " eh o maior\n";
+
+    return 0;
+}
