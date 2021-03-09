15053d0c2fba40096ef4aed47fa85f053c66560c
Guga <54151974+gustavo-tomas@users.noreply.github.com>
2021-03-09 17:31:20 -0300

    gandalf carrying the team

---
+++ 3145.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    int a, b;
+    cin >> a >> b;
+    cout << fixed << setprecision(2) << (double) b / (a+2) << endl;
+ 
+    return 0;
+}
