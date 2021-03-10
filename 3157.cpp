3518b7e41e1b9cd7059148df1ec5385b8329a4a7
Guga <54151974+gustavo-tomas@users.noreply.github.com>
2021-03-10 14:36:23 -0300

    coronguinha

---
+++ 3157.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    string bulshiet;
+    getline(cin, bulshiet);
+    cout << "Nao se refuta ciencia com opiniao" << endl;
+
+    return 0;
+}
