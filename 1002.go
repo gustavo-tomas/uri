95bee53e506207a839aa625bc3d1f5df49aae08b
Gustavo Tomás <guga.tomas10@gmail.com>
2021-08-05 18:55:42 -0300

    going up

---
+++ 1002.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+ 
+func main() {  
+   var r float64
+   fmt.Scanf("%f", &r)
+   fmt.Printf("A=%.4f\n",r*r*3.14159)
+}
---
+++ 1007.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+ 
+func main() {  
+   var a, b, c, d int64
+   fmt.Scanf("%d\n%d\n%d\n%d", &a, &b, &c, &d)
+   fmt.Printf("DIFERENCA = %d\n", (a*b - c*d))
+}
