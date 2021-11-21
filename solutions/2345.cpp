635172ce14955f55621c884319aa8eff75aca7aa
Guga <54151974+gustavo-tomas@users.noreply.github.com>
2021-11-21 19:10:15 -0300

    yes

---
+++ solutions/2345.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, min_dif = 1;
+    cin >> a >> b >> c >> d;
+    cout << abs((d + a) - (c + b)) << endl;
+    return 0;
+}
